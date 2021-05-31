c33670969b432d404c8d5239d235bb97cd359f93
debanjan19974 <85027539+debanjan19974@users.noreply.github.com>
2021-05-31 20:55:48 +0530

    Java Project

---
+++ JAVA Project/com.OnlineCalci/src/com/OnlineCalci/MultiplicationServlet.java
@@ -0,0 +1,20 @@
+package com.OnlineCalci;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MultiplicationServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req,HttpServletResponse resp) throws IOException {
+	 	int n1=Integer.parseInt(req.getParameter("num1")); //we take input as number hence int //getparameter takes string has input hence convert to string using Integer.parseInt
+	 	int n2=Integer.parseInt(req.getParameter("num2"));
+	 	int res=n1*n2;
+	 	PrintWriter out=resp.getWriter();
+		out.print("The Multiplication of "+n1+" and "+n2+" is "+res);
+
+
+}
+}
